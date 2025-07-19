a366ff3f7dca78ccd0a510f4c274031889d5a27c
bob240 <74813341+bob240@users.noreply.github.com>
2025-07-19 22:03:26 +0800

    Update and rename build.sh to build.sh

--- packages/mesa-vulkan-freedreno-25.3.0/build.sh
+++ packages/mesa-vulkan-freedreno-25.3.0/build.sh
@@ -1,4 +1,4 @@
-PKG_VER=25.2.0-devel
+PKG_VER=25.3.0-devel
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Turnip Driver"
 VK_DRIVER_LIB="libvulkan_freedreno.so"
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCH=x86_64
 
 GIT_URL=https://github.com/bob240/mesa-mirror
-GIT_COMMIT=a6c31d80cdd9d12e302ea533aa52430879d953cd
+GIT_COMMIT=eb207415120c087efb7bb9e97f23d67f6a93085e
 
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
